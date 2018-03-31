a400ad244dd6f18bfec317b0d72565383de8d639
Oleg Gashev <oleg@gashev.net>
2018-03-31 05:50:27 +0000

    Replaced list of fibonacci numbers to two variables.

--- project-euler/2/problem-2.js
+++ project-euler/2/problem-2.js
@@ -1,13 +1,12 @@
-var fib = [1, 1];
-
-var index = 1;
 var sum = 0;
 
-while (fib[index] <= 4000000) {
-    index++;
-    fib[index] = fib[index - 1] + fib[index - 2];
-    if (fib[index] % 2 == 0) {
-        sum += fib[index];
+var prev = 1;
+var current = 1;
+
+while (current <= 4000000) {
+    current = [current + prev, prev = current][0];
+    if (current % 2 == 0) {
+        sum += current;
     }
 } 
 
