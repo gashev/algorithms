1921fe6bfed52309d8b0580fa78f42fcc7f92ce2
Oleg Gashev <oleg@gashev.net>
2018-04-14 12:48:11 +0000

    Project euler number 7.

---
+++ project-euler/7/problem-7.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int isPrime(const unsigned int, const unsigned int*, const unsigned int);
+
+int main(void) {
+    const num = 10001;
+
+    unsigned int *primes;
+    primes = (unsigned int *) malloc(num * sizeof(unsigned int));
+
+    primes[0] = 2;
+    unsigned int primesCount = 1;
+
+    if (primes == NULL) {
+        puts("Can't allocate memory.");
+        return 1;
+    }
+
+    unsigned int tmp = 3;
+    while (primesCount < num) {
+        if (isPrime(tmp, primes, primesCount) == 1) {
+            primes[primesCount] = tmp;
+            primesCount++;
+        }
+        tmp += 2;
+    }
+    printf("%d\n", primes[primesCount - 1]);
+    free(primes);
+
+    return 0;
+}
+
+
+int isPrime(
+    const unsigned int num,
+    const unsigned int* primes,
+    const unsigned int count
+) {
+    unsigned int i = 0;
+    
+    while (i < count) {
+        if (num % primes[i] == 0) {
+            return 0;
+        }
+        i++;
+    }
+    
+    return 1;
+}
