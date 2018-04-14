31453fd67af9a3693d1b07008546117162bd37a0
Oleg Gashev <oleg@gashev.net>
2018-04-14 19:43:24 +0000

    project euler: improvment for problem 9.

--- project-euler/9/problem-9.js
+++ project-euler/9/problem-9.js
@@ -2,10 +2,11 @@
 
 function getPythagoreanTripletProduct(sum) {
     for (var a = 1; a < sum - 1; a++) 
-        for (var b = 1; b < sum - 1; b++) 
-            for (var c = 1; c < sum - 1; c++)
-                if ((a * a + b * b == c * c) && (a + b + c == sum))
-                    return (a * b * c);
+        for (var b = 1; b < sum - 1; b++) {
+            var c = sum - a - b;
+            if (a * a + b * b == c * c)
+                return (a * b * c);
+        }
 }
 
 console.log(getPythagoreanTripletProduct(1000));
