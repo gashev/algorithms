c53c18f1903ae3e1bb2fae1b3b3c091e46bb5916
Oleg Gashev <oleg@gashev.net>
2019-04-24 19:48:59 +0000

    Updated leetcode problem 1018.

--- leetcode/1018/1018.js
+++ leetcode/1018/1018.js
@@ -7,8 +7,8 @@ var prefixesDivBy5 = function(A) {
     var num = 0;
 
     for (let i = 0; i < A.length; i++) {
-        num = (num * 2 + A[i]) % 10;
-        result.push(num % 5 == 0);
+        num = (num * 2 + A[i]) % 5;
+        result.push(!num);
     }   
 
     return result; 
